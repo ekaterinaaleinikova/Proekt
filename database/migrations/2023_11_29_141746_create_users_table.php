687a301636e89aaaa36d58c2f097547512696da8
ekaterinaaleinikova <ekaterinaaleinikova61@gmail.com>
2023-11-30 16:04:44 +0600

    first commit

--- database/migrations/2023_11_29_141746_create_users_table.php
+++ database/migrations/2023_11_29_141746_create_users_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
+            $table->string('email');
+            $table->string('phone'); 
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('users');
     }
 };
